485a09ecb6fdfb0e98b01b3a0bf17265bf8ece3c
Jorge Luiz Fanderuff <64379265+jorgeluizfanderuff@users.noreply.github.com>
2021-11-15 22:09:42 -0300

    Versão R2 em 15 11 21

--- meu_site.py
+++ meu_site.py
@@ -14,6 +14,10 @@ def homepage():
 def contatos():
     return render_template("contatos.html")
 
+@app.route("/legislacao")
+def legislacao():
+    return render_template("legislacao.html")
+
 @app.route("/usuarios/<nome_usuario>")
 def usuarios(nome_usuario):
     return render_template("usuarios.html", nome_usuario=nome_usuario)
